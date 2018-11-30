6fc9656701956fd9549ecffab1dc22c0d6779ce2
Sagar Gupta <sgupta.8874@outlook.com>
2018-11-30 16:04:12 +0530

    Activity name changed MainActivity to PaymentActivity

--- app/src/main/java/com/sample/empasssamplelibrary/PaymentActivity.kt
+++ app/src/main/java/com/sample/empasssamplelibrary/PaymentActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class PaymentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
